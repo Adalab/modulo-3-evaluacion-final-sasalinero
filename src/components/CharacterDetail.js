d11897f1381f89c9b3ab15d628b9968872b1cf98
sasalinero <sasalinero@gmail.com>
2021-10-04 11:09:44 +0200

    new css

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -9,37 +9,41 @@ import "../styles/layout/StyleCharacterDetails.scss";
 import "../styles/layout/StyleCharacterItem.scss";
 
 const CharacterDetail = (props) => {
-  const estado = props.bicho.estadoDelBicho;
+  if (props.bicho === undefined) {
+    return <p>"No existe</p>;
+  } else {
+    const estado = props.bicho.estadoDelBicho;
 
-  const renderIcon = () => {
-    if (estado === "Alive") {
-      return <FontAwesomeIcon className="heart" icon={faHeart} />;
-    } else if (estado === "Dead") {
-      return <FontAwesomeIcon className="dead" icon={faSkullCrossbones} />;
-    } else return <FontAwesomeIcon className="unknown" icon={faQuestion} />;
-  };
+    const renderIcon = () => {
+      if (estado === "Alive") {
+        return <FontAwesomeIcon className="heart" icon={faHeart} />;
+      } else if (estado === "Dead") {
+        return <FontAwesomeIcon className="dead" icon={faSkullCrossbones} />;
+      } else return <FontAwesomeIcon className="unknown" icon={faQuestion} />;
+    };
 
-  return (
-    <div className="styleCharacterDetail">
-      <img
-        className="img"
-        src={props.bicho.fotoDelBicho}
-        alt="imageCharacterBicho"
-      />
-      <br />
-      <h2 className="texto"> Nombre:</h2>{" "}
-      <h2 className="nombreReal">{props.bicho.nombreDelBicho}</h2>
-      <br />
-      <h2>Especie: {props.bicho.especieDelBicho}</h2>
-      <br />
-      <h2>Episodios:{props.bicho.episodiosDelBicho}</h2>
-      <br />
-      <h2>
-        Estado: {props.bicho.estadoDelBicho}
-        {renderIcon()}
-      </h2>
-    </div>
-  );
+    return (
+      <div className="styleCharacterDetail">
+        <img
+          className="img"
+          src={props.bicho.fotoDelBicho}
+          alt="imageCharacterBicho"
+        />
+        <br />
+        <h2 className="texto"> Nombre:</h2>{" "}
+        <h2 className="nombreReal">{props.bicho.nombreDelBicho}</h2>
+        <br />
+        <h2>Especie: {props.bicho.especieDelBicho}</h2>
+        <br />
+        <h2>Episodios:{props.bicho.episodiosDelBicho}</h2>
+        <br />
+        <h2>
+          Estado: {props.bicho.estadoDelBicho}
+          {renderIcon()}
+        </h2>
+      </div>
+    );
+  }
 };
 
 export default CharacterDetail;
